6212bacca17dd7e2dfe539fc9981d721d776f797
ihemjica <ihemjica@gmail.com>
2022-09-12 12:27:54 -0700

    0x01

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Point of entry.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
