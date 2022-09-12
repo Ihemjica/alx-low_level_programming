97aa74e768f25f9d8719ef2f08476fd5c4294144
ihemjica <ihemjica@gmail.com>
2022-09-12 13:02:38 -0700

    0x01

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of three different digits.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int no1, no2, no3;
+
+	for (no1 = 0; no1 < 8; no1++)
+	{
+		for (no2 = no1 + 1; no2 < 9; no2++)
+		{
+			for (no3 = no2 + 1; no3 < 10; no3++)
+			{
+				putchar((no1 % 10) + '0');
+				putchar((no2 % 10) + '0');
+				putchar((no3 % 10) + '0');
+
+				if (no1 == 7 && no2 == 8 && no3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
