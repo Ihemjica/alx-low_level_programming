b81c22424564b472f9eba0f5c09628fbfd0716c8
ihemjica <ihemjica@gmail.com>
2022-09-12 12:32:50 -0700

    0x01

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Point of entry.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+{
+	putchar(num);
+	if (num != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+}
+	putchar(10);
+	return (0);
+}
