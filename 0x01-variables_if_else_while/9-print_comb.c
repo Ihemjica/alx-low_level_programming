1ff954546bfba4ec36a88dc7347d7c78a76e17d1
Ihemjica <ihemjica@gmail.com>
2023-03-19 17:41:33 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@ int main(void)
 	int num;
 
 	for (num = 48; num < 58; num++)
-{
-	putchar(num);
-	if (num != 57)
+	{
+		putchar(num);
+		if (num != 57)
 		{
 			putchar(44);
 			putchar(32);
 		}
-}
+	}
 	putchar(10);
 	return (0);
 }
